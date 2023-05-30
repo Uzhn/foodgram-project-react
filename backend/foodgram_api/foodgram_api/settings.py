ee40cf5843d35d72cf296efe85338724ed7db7ad
Uzhn <evgeniirg73@gmail.com>
2023-05-30 18:00:20 +0700

    db3

--- backend/foodgram_api/foodgram_api/settings.py
+++ backend/foodgram_api/foodgram_api/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'foodgram_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
